890ba447e7b08a662225f508319a6d55cb59f2b7
David Lexe <David_Lexe@hotmail.com>
2025-03-28 10:05:46 +0100

    Del 4

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,21 @@ public class Main{
         System.out.println("Welcome to Skola. \n" +
                         "Please enter something: ");
         String input = sc.nextLine();
-        System.out.println(input);
 
-        run();
+        run(input);
     }
 
-    public static void run(){
-        Cem cem = new Cem();
-        Lexe lexe = new Lexe();
-        Carl carl = new Carl();
-        cem.hello();
-        lexe.hello();
-        carl.hello();
+    public static void run(String input) {
+        if(input.equals("Carl")) {
+            Carl carl = new Carl();
+        } else if(input.equals("Lexe")) {
+            Lexe lexe = new Lexe();
+            lexe.hello();
+        } else if(input.equals("Cem")) {
+            Cem cem = new Cem();
+            cem.hello();
+        } else{
+            System.out.println("Something went wrong");
+        }
     }
 }
