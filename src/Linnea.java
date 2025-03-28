054f299aeeb23ad6b527f4ef487d53dc16fe28b4
Linnea Värnström <linnea.varnstrom@outlook.com>
2025-03-28 10:22:45 +0100

    Merge branch 'linneas-second-branch'

--- src/Linnea.java
+++ src/Linnea.java
@@ -1,5 +1,10 @@
 public class Linnea {
-    public void linnea() {
+    public void food() {
         System.out.println("66666");
     }
+
+    public void drink(){
+        System.out.println("99999")
+    }
 }
+
