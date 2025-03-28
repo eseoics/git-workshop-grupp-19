fe90f6b6afcec0528589be79f14313af529fcd25
Carl Lundholm <carl_0221@hotmail.se>
2025-03-28 09:56:17 +0100

    Update Carl.java

--- src/Carl.java
+++ src/Carl.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
 
 public class Carl {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Welcome to Carls class!");
-        System.out.println("Please enter your name: \n");
-        String name = sc.nextLine();
-
-        System.out.println("Hello " + name + "!");
+    public Carl() {
+        hello();
+    }
+    public void hello() {
+        System.out.println("Hello, I am Carl.");
     }
 }
