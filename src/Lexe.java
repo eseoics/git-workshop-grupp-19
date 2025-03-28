895c6229acc7a6e262ee80231b04d70852c2dcf7
David Lexe <David_Lexe@hotmail.com>
2025-03-28 10:09:05 +0100

    Added method favourite food

--- src/Lexe.java
+++ src/Lexe.java
@@ -2,4 +2,8 @@ public class Lexe {
     public void hello() {
         System.out.println("Lexe was here!");
     }
+
+    public void food(){
+        System.out.println("My favourite food is burritos.");
+    }
 }
