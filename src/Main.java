bd6438181f5433b11b704d494f14ec1eb633f58d
Carl Lundholm <carl_0221@hotmail.se>
2025-03-28 09:54:52 +0100

    Revert "Refactored carl Class"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class Main{
-    public void carl(){
-        System.out.println("Hello from Carl!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Skola. \n" +
+                        "Please enter something: ");
+        String input = sc.nextLine();
+        System.out.println(input);
     }
 }
